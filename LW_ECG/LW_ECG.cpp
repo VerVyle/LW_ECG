1b2fc25e67b7f3b153f7b21cf90b3be29e2b137e
VerVyle <62177496+VerVyle@users.noreply.github.com>
2023-03-25 16:13:28 +0500

    Update LW_ECG.cpp

--- LW_ECG/LW_ECG.cpp
+++ LW_ECG/LW_ECG.cpp
@@ -1,12 +1,11 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include "math_3d.h"
 
 GLuint VBO;
 
-static void RenderSceneCB()
-{
+static void RenderSceneCB() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glEnableVertexAttribArray(0);
@@ -17,17 +16,14 @@ static void RenderSceneCB()
 
     glDisableVertexAttribArray(0);
 
-    glutSwapBuffers();
+    glutSwapBuffers(); // отрисовка
 }
 
-
-static void InitializeGlutCallbacks()
-{
-    glutDisplayFunc(RenderSceneCB);
+static void InitializeGlutCallbacks() {
+    glutDisplayFunc(RenderSceneCB); // Эта функция регулярно вызывается GLUT'ом.
 }
 
-static void CreateVertexBuffer()
-{
+static void CreateVertexBuffer() {
     Vector3f Vertices[3];
     Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
     Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
@@ -38,29 +34,26 @@ static void CreateVertexBuffer()
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
-
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+int main(int argc, char** argv) {
+    glutInit(&argc, argv); // Инициализация OpenGL utility library с параметрами из командной строки
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // GLUT_DOUBLE включает двойную буферизацию, GLUT_RGBA - буфер цвета, непрерывно используемого для конечной цели рендеринга (т.е. на экране).
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 03");
+    glutCreateWindow("Task 3"); // Эти функции задают параметры окна и создают его. Так же мы даем ему заголовок.
 
-    InitializeGlutCallbacks();
+    InitializeGlutCallbacks(); // функция обратного вызова.
 
-    // Must be done after glut is initialized!
     GLenum res = glewInit();
     if (res != GLEW_OK) {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
-    }
+    } // Теперь мы инициализируем GLEW и проверяем на ошибки. GLUT необходимо инициализировать перед этим.
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // устанавливает цвет, который будет использован во время очистки буфера кадра.
 
-    CreateVertexBuffer();
+    CreateVertexBuffer(); // создание треугольника происходит здесь.
 
-    glutMainLoop();
+    glutMainLoop(); // передача управления GLUT.
 
     return 0;
 }
