c7e5de2b7c9d466e4495da6ee939810b4a5d7fdb
VerVyle <62177496+VerVyle@users.noreply.github.com>
2023-04-29 11:20:58 +0500

    Add files via upload

---
+++ LW1/tasks.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef TASKS_H
+#define	TASKS_H
+
+void invoke1(int argc, char** argv);
+void invoke2(int argc, char** argv);
+void invoke3(int argc, char** argv);
+void invoke4(int argc, char** argv);
+
+#endif	/* TASKS_H */
